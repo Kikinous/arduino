eb9a2bed995c8c532a5dfb70d84552561edab368
Kikinous <julienborghetti@gmail.com>
2016-12-19 21:21:57 +0100

    version beta

---
+++ ReceiveSMS_relay.ino
@@ -0,0 +1,116 @@
+#include <GSM.h>
+#define PINNUMBER ""
+
+GSM gsmAccess;
+GSM_SMS sms;
+
+char senderNumber[20];
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("SMS Messages Receiver");
+
+  boolean notConnected = true;
+
+  while (notConnected) {
+    if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
+      notConnected = false;
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+
+  Serial.println("GSM initialized");
+  Serial.println("Waiting for messages");
+
+  pinMode(13, OUTPUT);  
+  digitalWrite(13, LOW);
+
+
+  sms.beginSMS("+33673997334");
+//  sms.beginSMS("+33787025242");
+  sms.print("Relai actif ; pret a recevoir instructions ('F' pour OFF ou 'N' pour ON)");
+  sms.endSMS();
+  Serial.println("\nMessage envoye!\n");
+
+
+}
+
+void loop() {
+  char c;
+
+  // If there are any SMSs available()
+  if (sms.available()) {
+    Serial.println("Message received from:");
+
+    // Get remote number
+    sms.remoteNumber(senderNumber, 20);
+    Serial.println(senderNumber);
+
+    // An example of message disposal
+    // Any messages starting with # should be discarded
+    if (sms.peek() == '#') {
+      Serial.println("Discarded SMS");
+      sms.flush();
+    }
+
+    // Read message bytes and print them
+    boolean i = true;
+    char code;
+    while (c = sms.read()) {
+      if( i == true ){
+        code = c;
+        i = false;
+      }
+      Serial.print(c);
+    }
+    Serial.println("\nEND OF MESSAGE");
+
+    // Delete message from modem memory
+    sms.flush();
+    Serial.println("MESSAGE DELETED");
+
+    // Agit et repond
+    sms.beginSMS(senderNumber);
+    switch (process_code(code)){
+      case 0:
+        sms.print("Extinction du relai -- recu");
+        break;
+      case 1:
+        sms.print("Allumage du relai -- recu");
+        break;
+      case 2:
+        sms.print("Juste un caractere : 'N' pour ON ou 'F' pour OFF");
+        break;
+    }
+    sms.endSMS();
+    Serial.println("\nMessage envoye!\n");
+  } 
+
+
+  delay(1000);
+
+}
+
+int process_code(char code){
+  /* // CLIGNOTEMENT POUR DEBUG
+  for(int i=0;i<10;i++){
+    digitalWrite(13, HIGH);
+    delay(50);
+    digitalWrite(13, LOW);
+    delay(50);
+  } 
+ */
+  if (code == 'F'){
+    digitalWrite(13, LOW);    
+    return 0;
+   }
+  else if (code == 'N'){
+    digitalWrite(13, HIGH);    
+    return 1;
+  }
+  else{
+    return 2;
+  }
+}
